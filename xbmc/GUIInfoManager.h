bc78e30feef2ae4a836814644357c829b157cd8d
xhbl <newxhbl@hotmail.com>
2014-08-23 15:56:05 +0800

    [DualAudio] version bump to 13.2-DualAudio-r019

--- xbmc/GUIInfoManager.h
+++ xbmc/GUIInfoManager.h
@@ -498,7 +498,7 @@ namespace INFO
 
 #define VERSION_MAJOR               13
 #define VERSION_MINOR               2
-#define VERSION_TAG                 "-DualAudio-r018"
+#define VERSION_TAG                 "-DualAudio-r019"
 
 #define LISTITEM_START              35000
 #define LISTITEM_THUMB              (LISTITEM_START)
